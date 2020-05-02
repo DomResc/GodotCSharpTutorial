32a84cc507444c08239268cc0af46b4c6ab3ba94
Domenico Rescigno <domenico.rescigno@gmail.com>
2020-05-02 18:38:08 +0200

    Commit EP2

---
+++ EP2/Scripts/Panel.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class Panel : Godot.Panel
+{
+    Node2D sprite;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        sprite = GetNode<Node2D>("Sprite");
+
+        GetNode("Button").Connect("pressed", this, nameof(OnButtonPressed));
+    }
+
+    // Called when we click the button
+    public void OnButtonPressed()
+    {
+        Random random = new Random();
+
+        int positionX = random.Next(1, 1024);
+        int positionY = random.Next(1, 600);
+
+        sprite.Position = new Vector2(positionX, positionY);
+
+        GetNode<Label>("Label").Text = sprite.Position.ToString();
+    }
+}
